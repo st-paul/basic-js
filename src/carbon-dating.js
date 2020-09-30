07b47500814106914b23f47c7731b3c7491dd68e
st-paul <statkevich.pa@gmail.com>
2020-10-01 01:56:07 +0300

    feat: implement task count-cats and carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,17 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) === 'string' 
+        && sampleActivity.trim().length !== 0 
+        && /^\d*\.?\d+$/.test(sampleActivity) 
+        && parseFloat(sampleActivity) > 0 
+        &&  parseFloat(sampleActivity) < 15) {
+          let ActNum = parseFloat(sampleActivity);
+          let k = Math.LN2 / HALF_LIFE_PERIOD;
+            let result = Math.log((MODERN_ACTIVITY / ActNum)) / k;
+              return Math.ceil(result);
+  } else {
+    return false;
+  }
+}
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let sumCat = 0;
+  backyard.reduce((acc, curval) => curval.reduce((mem, curr) => curr === '^^' ? sumCat++: mem, 0), 0);
+  return sumCat;
 };
