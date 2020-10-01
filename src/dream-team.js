cb3a134dfd5d5b014e6b8bb7255a61b487875b31
st-paul <statkevich.pa@gmail.com>
2020-10-02 00:11:31 +0300

    feat: implement dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let resultStr = [];
+    if (Array.isArray(members) === true) {
+        members.some((val) => typeof val === 'string' ? val.trim().toUpperCase().split('').findIndex((el, index) => el === el[0] ? resultStr.push(el) : false) : false);
+    return resultStr.sort().join('');
+  } return false;
 };
