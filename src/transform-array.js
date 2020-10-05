b25a70be1c7665d67f46c2cfb9fc22942a485d3d
st-paul <statkevich.pa@gmail.com>
2020-10-05 14:29:18 +0300

    feat: implement task hanoi and transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let resultArr = [];
+  let index = 0;
+  if (Array.isArray(arr)) {
+    while (index < arr.length) {
+      if (arr[index] === '--discard-next') {
+        arr[index + 2] ===  '--discard-prev' || arr[index + 2] ==  '--double-prev' ? index += 3 : index += 2;
+      } else if (arr[index] === ('--discard-prev')) {
+        index > 0 ? (resultArr.pop(), index++) : index++;
+      } else if (arr[index] === '--double-next') {
+        index < arr.length - 1 ? (resultArr.push(arr[index + 1]), index++) : index++;
+      } else if (arr[index] === '--double-prev') {
+        index > 0 ? (resultArr.push(arr[index - 1]), index++) : index++;
+      } else {
+        resultArr.push(arr[index]);
+        index++;
+      }
+    }
+  } else {
+    throw new Error('THROWN');
+  }
+  return resultArr;
 };
